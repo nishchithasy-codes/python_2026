0a27046c9fe1c2727185f9865670ed842dc5b43a
nishchithasy <nishchithayogeshnishchithasy@gmail.com>
2026-03-03 22:22:07 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,8 @@
+boy_name = input("Enter boy name: ")
+girl_name = input("Enter girl name: ")
+boy_age = int(input("Enter boy age>>"))
+girl_age = int(input("Enter girl age>>"))
+age_diff = abs(boy_age - girl_age)          #abs is for absolute value if minus sign comes
+print(f"{boy_name} loves {girl_name}.And their age difference is {age_diff} year ")
+
+
