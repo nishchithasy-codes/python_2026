0face22aa91b03c1501db7fa44e3e05e52444a87
nishchithasy <nishchithayogeshnishchithasy@gmail.com>
2026-03-03 22:20:52 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,70 @@
+'''
+x = 5
+print(type(x))      #output: <class'integer'>
+'''
+'''
+x = "10"            # x is a string
+y = int(x)          #convert string to integer
+z = float(y)        #convert integer to float
+print(x)
+'''
+'''
+
+//  floor division  -celling,floor like that but to print the floor value that is the first number of the value we are using floor division
+%   modulus
+**  exponentiation
+/   division 
+*   multiplication
++   addition
+-   subtraction
+'''
+
+a = 10
+b = 3
+print(a + b)        #addition
+print(a - b)        #subtraction
+print(a / b)        #division
+print(a * b)        #multiplication
+print(a % b)        #modulus
+print(a ** b)       #exponentiation i.e., 10^3
+print(a // b)       #floor division
+
+age = 18            #int
+is_student = False  #bool
+Weight = 65.5       #float
+print(type(Weight)) 
+print(type(is_student))
+print(type(age))
+
+is_student = "yes"      #str
+print(type(is_student))
+
+age_float = float(18)      #to convert age to float
+print(age_float)
+
+s = '100'
+print(int(s) + age)
+
+a,b = b,a               #for swaping the variables
+print(a,b)              #now output is a = 3 and b = 10
+
+
+
+
+
+
+
+""
+
+'''
+x, y, z = 10, 20, 30
+print(x)        #output is 10
+print(y)        #output is 20
+print(z)        #output is 30
+'''
+
+'''
+x = y = z = 100
+print(x, y, z)        #output is 100
+'''
+
