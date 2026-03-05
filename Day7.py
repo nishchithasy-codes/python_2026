9864002c42a89a6a8aeeeb31157eafa15a25b50f
nishchithasy <nishchithayogeshnishchithasy@gmail.com>
2026-03-05 22:00:49 +0530

    Add files via upload

---
+++ Day7.py
@@ -0,0 +1,36 @@
+#Tuples and sets in python
+#For tuples we use() and for sets we use {}
+'''fruits =("Apple", "Orange", "Banana", "Goa", "Apple")
+print(fruits)
+fruits =("Apple", "Orange", "Banana", "Goa", "Apple")*3          #Tuples allow duplicate values also
+print(fruits)
+print(fruits[0])
+print(fruits[1:4])
+print(len(fruits))
+print(fruits.count("Apple"))
+print(fruits.index("Apple"))                #it will print the value of first apple only
+print("cashew" in fruits)           #false bcz no cashew in the tuple
+print(fruits)
+
+#combined tuple
+print(f"combined tuples")
+tuple1 = (1,2,3,4,5)
+tuple2 = (6,7,8,9,10)
+tuples = tuple1 + tuple2
+print(tuples)'''
+
+#Sets no duplicate values, unordered, mutable and unindexed
+'''set1 = {1,2,3,4,5,9}
+print(set1.pop())           #-> it removes the random element bcz set is unindexed
+print(set1.clear())
+#print(set1.remove(set1,4))       #removes 4 from set1
+#print(set1.remove(10))      #crash the program bcz no value of 10 in set1
+print(set1.discard(7))         #if present it romeves otherwise simply ignore it
+
+set2 = {6,7,8,9,10,3,}
+print(set2.add(3))
+print(set2.clear())
+#print(set2.pop())
+#print(set2.remove(6))
+print(set2.discard(2))'''
+
