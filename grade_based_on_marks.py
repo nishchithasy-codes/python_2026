24368f640c10751db8c4d954b6394003a097a830
Nishchitha S Y <240329141+nishchithasy-codes@users.noreply.github.com>
2026-04-04 22:06:04 +0530

    Add files via upload

---
+++ grade_based_on_marks.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks:"))
+
+if marks >= 90:
+    print("Marks = ",marks,"Grade A")
+elif marks >=85:
+    print("Marks = ",marks,"Grade B")
+elif marks >=70:
+    print("Marks = ",marks,"Grade C")
+elif marks >=65:
+    print("Marks = ",marks,"Grade D")
+else:
+    print("Marks = ",marks,"Grade F")
