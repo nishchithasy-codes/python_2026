d733d0fcbeeb94142d7614e62ddb498cf96c1cdb
Nishchitha S Y <240329141+nishchithasy-codes@users.noreply.github.com>
2026-04-17 23:15:18 +0530

    Add files via upload

---
+++ Day34.py
@@ -0,0 +1,4 @@
+file = open("notes2.txt","w")
+print(file.write("\nEvery expert in Python was once confused by indentation—keep going.\n"))
+print(file.write("Write code daily, even small lines build big skills."))
+file.close()
